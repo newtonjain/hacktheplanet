307a668d8d702a05e888ba16a218e6cfc4009a44
aleccool213 <moteygo@gmail.com>
2015-08-15 20:40:38 -0700

    someone fucked up royally

---
+++ utsu_bulletinboard/api/migrations/0007_auto_20150816_0058.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('api', '0006_auto_20150815_2258'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='trip',
+            name='status',
+            field=models.CharField(max_length=200, blank=True, default='unconfirmed'),
+        ),
+    ]
