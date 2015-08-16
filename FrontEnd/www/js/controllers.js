6f123ecc80e6fd2260bcce8181739168794ab790
Newton Jain <newton.jain@gmail.com>
2015-08-16 07:39:02 -0400

    Created drivers page

--- FrontEnd/www/js/controllers.js
+++ FrontEnd/www/js/controllers.js
@@ -74,11 +74,12 @@ angular.module('starter.controllers', [])
   }
     //document.addEventListener('deviceready', this.onDeviceReady, false);
   $scope.tapping = function(amount){
-    //alert('amount'  + amount);
+    alert('amount'  + amount);
 
     $scope.amount = amount;
     nfc.addTagDiscoveredListener(triggerEvent,
       function () {
+        alert('I am here');
       $scope.hideSheet = $ionicActionSheet.show({
           buttons: [
        { text: '<center><b>You are going to pay $' + $scope.amount}
