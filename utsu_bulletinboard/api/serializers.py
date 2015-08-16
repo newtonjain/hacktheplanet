a2b3747ff3730f45e79b7cbf936846d9de076fa0
aleccool213 <moteygo@gmail.com>
2015-08-16 06:07:27 -0700

    k no texting right now

--- utsu_bulletinboard/api/serializers.py
+++ utsu_bulletinboard/api/serializers.py
@@ -5,7 +5,7 @@ from api.models import Trip, Route
 from users.models import User
 
 from api.utils.utils import scenic_trip_builder
-from api.utils import (
+from api.utils.utils import (
     send_arrived,
     send_confirmed,
     send_unconfirmed
--- utsu_bulletinboard/api/utils/sendsms.py
+++ utsu_bulletinboard/api/utils/sendsms.py
@@ -1,21 +1,21 @@
 
 # Download the Python helper library from twilio.com/docs/python/install
-from twilio.rest import TwilioRestClient
-account_sid = "AC7f33789db90afa45749656926700a7f1"
-auth_token  = "63a1850c200e719d08c0d0c343d47e54"
-client = TwilioRestClient(account_sid, auth_token)
+# from twilio.rest import TwilioRestClient
+# account_sid = "AC7f33789db90afa45749656926700a7f1"
+# auth_token  = "63a1850c200e719d08c0d0c343d47e54"
+# client = TwilioRestClient(account_sid, auth_token)
 
 from users.models import User
 
 
 class SMS(object):
     def __init__(self, number):
-    	""" Initilializes SMS  """
-      	self.number = "+" + str(number)
+        """ Initilializes SMS  """
+        self.number = "+" + str(number)
 
 
     def send(self, body_text):
-    	""" Sends SMS """
-    	message = client.sms.messages.create(body=str(body_text), to=self.number, from_="+13853557433")
-		return message.sid
-
+        """ Sends SMS """
+        # message = client.sms.messages.create(body=str(body_text), to=self.number, from_="+13853557433")
+        # return message.sid
+        pass
