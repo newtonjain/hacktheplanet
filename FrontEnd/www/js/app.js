f0871a6d42c55693b78d67102b30b220e575c7bb
Newton Jain <newton.jain@gmail.com>
2015-08-16 13:35:52 -0400

    Frontend workd

--- FrontEnd/www/js/app.js
+++ FrontEnd/www/js/app.js
@@ -100,6 +100,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
   });
 
   // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+  $urlRouterProvider.otherwise('/tab/account');
 
 });
