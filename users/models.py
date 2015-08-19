2b7c9fe3d9ed17585e4fa8056ac7a69090869fb9
theshteves <kneisers@msu.edu>
2015-08-19 12:35:52 -0700

    Alright alec, let me know if I'm onto something

--- users/models.py
+++ users/models.py
@@ -8,30 +8,40 @@ from django.core.urlresolvers import reverse
 from django.db import models
 from django.utils import timezone
 
-from api.models import Trip
+# Should be seperate but for clarity I'm putting them together to showcase our models
+# from api.models import Trip
+
 # from django.utils.translation import ugettext_lazy as _
 
 
 class Trip(models.Model):
     """ Trip model """
 
+    trip_id = models.BigIntegerField(blank=True, null=True)
 	scenic = models.BooleanField(blank=True, default=False)
 	start_ts = models.DateTimeField(blank=True, null=True, default=timezone.now)
 	status = models.CharField(blank=True, default='unconfirmed', max_length=200)
+    # Either distance or time to one-to-one with driver
+    # We could reward drivers based on distance
+    distance = models.BigIntergerField(blank=True, null=True)
+    driver = ForeignKey(Driver)
+    rider = ForeignKey(Rider)
+    # Unless driver and rider could be removed and solely linked through their trip_id
+    # Trip should definitely include addtional parameters
 
     # Why isn't this a thing?
     # def __unicode__(self):
-    #     pass some shit
+    #     return some machine-readble shit
 
 
-class User(models.Model):
-    """ User model """
+class User(model.Model):
+    """ [Abstract] User model """
+
+    class Meta:
+        abstract = True
 
     name = models.CharField(blank=True, max_length=100)
     phone_number = models.BigIntegerField(blank=True, null=True)
-    photo = models.URLField(blank=True)
-    description = models.CharField(blank=True, max_length=1000)
-    interests = models.CharField(blank=True, max_length=500)
 
     def __unicode__(self):
         """ Retuns username (utf-8) """
@@ -41,17 +51,21 @@ class User(models.Model):
         return reverse('users:detail', kwargs={'username': self.username})
 
 
-
-class Driver(models.Model):
+class Driver(User):
     """ Driver model """
 
-    user = models.ForeignKey(User)
-    # Drivers definitely should have their own additional fields
+    photo = models.URLField(blank=True)
+    # Instead of description and interests we should just have a bio
+    description = models.CharField(blank=True, max_length=1000)
+    interests = models.CharField(blank=True, max_length=500)
+    # As well as a field to showcase all of the trips driven
+    trips = ForeignKey(Trip)
+    bike = models.CharField(blank=True, max_length=100)
 
 
-class Rider(models.Model):
+class Rider(User):
     """ Rider model """
 
-    user = models.ForeignKey(User)
     # Might be useless but I'll leave it here in case I think of
     # something useful for it
+    pass
