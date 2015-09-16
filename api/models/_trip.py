a91c8547d71503bfcf6d638788d1eb3d3696f11b
aleccool213 <moteygo@gmail.com>
2015-09-16 19:45:47 -0400

    LOL dont know whats up yet

--- api/models/_trip.py
+++ api/models/_trip.py
@@ -5,10 +5,10 @@ from django.utils import timezone
 
 
 class Trip(models.Model):
-	# real fields
-	scenic = models.BooleanField(blank=True, default=False)
-	start_ts = models.DateTimeField(blank=True, null=True, default=timezone.now)
-	status = models.CharField(
-		blank=True, 
-		default='unconfirmed',
-		max_length=200)
+    # real fields
+    scenic = models.BooleanField(blank=True, default=False)
+    start_ts = models.DateTimeField(blank=True, null=True, default=timezone.now)
+    status = models.CharField(
+        blank=True,
+        default='unconfirmed',
+        max_length=200)
