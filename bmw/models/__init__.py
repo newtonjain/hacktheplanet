5ce7df41516117586607c4edae28e92afe5f458c
aleccool213 <moteygo@gmail.com>
2015-09-18 01:04:41 -0400

    imports

--- bmw/models/__init__.py
+++ bmw/models/__init__.py
@@ -1,5 +1,5 @@
 from ._trip import Trip # noqa
-from ._customer import Customer #noqa
-from ._driver import Driver #noqa
-from ._common import TripStatus #noqa
-from ._transaction import Transaction #noqa
+from ._customer import Customer # noqa
+from ._driver import Driver # noqa
+from ._common import TripStatus # noqa
+from ._transaction import Transaction # noqa
