aa90635224710a4f80a8f2ac87899051a353b4b4
aleccool213 <moteygo@gmail.com>
2015-09-19 13:42:17 -0400

    url stuff

--- config/settings/common.py
+++ config/settings/common.py
@@ -30,7 +30,6 @@ DJANGO_APPS = (
 
     # Useful template tags:
     'django.contrib.humanize',
-    'crispy_forms',
 
     # Admin
     'django.contrib.admin',
@@ -41,6 +40,7 @@ THIRD_PARTY_APPS = (
     'allauth',  # registration
     'allauth.account',  # registration
     'allauth.socialaccount',  # registration
+    'crispy_forms',
 )
 
 # Apps specific for this project go here.
