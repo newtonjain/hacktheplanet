51d8c0d442304782e0b17fdbe4d2601046596d9f
aleccool213 <moteygo@gmail.com>
2015-09-20 03:39:46 -0400

    lol

---
+++ yelp.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export CONSUMER_KEY='f8VIsslXwpsezEV4JrwFZw'
+export CONSUMER_SECRET='FsLUmb2uX4LcuA0FPwlSzhJxuNk'
+export TOKEN='0P-cl9sRfcOyr4D0Pp005q_Y0rmI_yU-'
+export TOKEN_SECRET='uOdWDlHzyQ-qzaqMg7YEM2yV3Iw'
